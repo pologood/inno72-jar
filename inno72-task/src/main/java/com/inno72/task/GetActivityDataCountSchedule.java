7fb068406fa633c9366750d0aecac593a39b050c
赵飞龙 <zhaofeilong@inno72.com>
2018-08-13 18:15:54 +0800

    提交定时

---
+++ inno72-task/src/main/java/com/inno72/task/GetActivityDataCountSchedule.java
@@ -0,0 +1,29 @@
+package com.inno72.task;
+
+import com.inno72.common.TaskProperties;
+import com.inno72.plugin.http.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableScheduling
+public class GetActivityDataCountSchedule {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Resource
+    private TaskProperties taskProperties;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void addData(){
+        logger.info("执行插入活动统计数据定时开始");
+        String url = taskProperties.getProps().get("setActivityDataCount");
+        HttpClient.get(url);
+        logger.info("执行插入活动统计数据定时结束");
+    }
+}
